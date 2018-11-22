8a3759e7841ff02aba1f611021e46920e494fc84
ElliotDoe <43886588+ElliotDoe@users.noreply.github.com>
2018-11-22 02:27:59 +0100

    Elliot's Vertex class
    Everything used to be in the testing code but it makes more sense to add it to the vertex. I didn't understand the last vertex class so I made my own. We can later use the Adjacency Matrix class to create the lines between the vertices (Using getX + getY from circle class which is it's superclass)

--- Vertex.java
+++ Vertex.java
@@ -1,83 +1,80 @@
 import java.lang.Math;
+import javafx.application.Application; 
+import static javafx.application.Application.launch; 
+import javafx.event.EventHandler;
+import java.awt.MouseInfo;
+import java.awt.PointerInfo;
+import java.awt.geom.Point2D;
+import java.awt.Point;
+import javafx.scene.Group; 
+import javafx.scene.Scene; 
+import javafx.scene.input.MouseEvent; 
+import javafx.scene.paint.Color; 
+import javafx.scene.shape.Circle; 
 
-public class Vertex 
+import java.util.EventObject;
+import javafx.event.Event;
+import javafx.event.ActionEvent;
+
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.layout.Region;
+import javafx.scene.control.Control;
+import javafx.scene.control.ComboBoxBase;
+import javafx.scene.control.ColorPicker;
+
+import javafx.scene.text.Font; 
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text; 
+import javafx.stage.Stage; 
+         
+import java.lang.Object;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Control;
+import javafx.scene.control.ChoiceBox;
+
+import java.lang.Object;
+import javafx.stage.Window;
+import javafx.stage.PopupWindow;
+import javafx.scene.control.PopupControl;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+
+public class Vertex extends Circle
 {
-	public Vertex(){
-	
-	color = 0;// one color for every vertex at the beggining
-	count++;
-	index=count;
-	
-		if (index==1){
-			A = new double[1][2];
-			x_coordinate = Math.random() * WIDTH + 1;
-			y_coordinate = Math.random() * HEIGHT +1;
-			x_coordinate = x_coordinate - (x_coordinate%5);
-			y_coordinate = y_coordinate - (x_coordinate%5);
-			
-			A[0][0] = x_coordinate;
-			A[0][1] = y_coordinate;
-		}
-	
-		else{
-			boolean check = false;
-			int samePosition=0;
-			
-			while (check==false){
-				x_coordinate = Math.random() * WIDTH + 1;
-				y_coordinate = Math.random() * HEIGHT + 1;
-					for (int i=0; i<A.length; i++){
-						if (A[i][0] == x_coordinate &&  A[i][1] == y_coordinate){
-							samePosition++;
-						}
-					}
-				
-					if (samePosition != 0){
-						check = false;
-					}
-					else{
-						double[][] newA = new double[A.length+1][1];
-						System.arraycopy(A,0,newA,0, A.length);
-						A = newA;
-						A[A.length-1][0]=x_coordinate;
-						A[A.length-1][1]=y_coordinate;
-						check = true;
-					}
-			}
-		}
-	
-	}
-
-	public void setColor(int c){
-		color = c;
-	}
-	
-	public int getColor(){
-		return color;
-	}
-	
-	public double getX(){
-		return x_coordinate;
-	}
-	
-	public double getY(){
-		return y_coordinate;
-	}
-	
-	public static double getRadius(){
-		return radius;
-	}
-	
-	
-	private double x_coordinate;
-	private double y_coordinate;
-	private int color;
+	private static int counter = 0;
 	private int index;
+	private final static double WIDTH = 600;
+	private final static double HEIGHT = 300;
+	private final static double RADIUS = 10;
+	public static String[] colorArray = ColorArray.getColorArray();
 	
-	private static int count;
-	private static double[][]A;
-	private static final double WIDTH = 400;
-	private static final double HEIGHT = 400;
-	private static final double radius = 5;
+	public Vertex(){
+		super(RADIUS);
+		setCenterX(Math.random()*WIDTH);
+		setCenterY(Math.random()*HEIGHT);
+		index = counter;
+		counter++;
 
-}
+		ContextMenu colorMenu = new ContextMenu();
+		MenuItemArray temp = new MenuItemArray();
+		MenuItem[] menuItemArray = temp.getMenuItemArray();
+		
+		for(int i=0; i<91;i++){
+		colorMenu.getItems().addAll(menuItemArray[i]);
+		}
+		
+		this.setOnMouseClicked((new EventHandler<MouseEvent>(){
+		@Override
+		public void handle(MouseEvent e){
+		PointerInfo a = MouseInfo.getPointerInfo();
+		Point b = a.getLocation();
+		colorMenu.show(Testing.root,b.getX(),b.getY());
+		}}));
+		}
+		
+	}
