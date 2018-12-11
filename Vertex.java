b1c27c1b03f728ba3610018b8ac0b9f452e18331
ElliotDoe <43886588+ElliotDoe@users.noreply.github.com>
2018-12-11 01:55:09 +0100

    Update Vertex.java

--- Vertex.java
+++ Vertex.java
@@ -1,33 +1,41 @@
 import java.lang.Math;
-import javafx.application.Application; 
-import static javafx.application.Application.launch; 
+import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.EventHandler;
 import java.awt.MouseInfo;
 import java.awt.PointerInfo;
-import java.awt.geom.Point2D;
 import java.awt.Point;
-import javafx.scene.Group; 
-import javafx.scene.Scene; 
-import javafx.scene.input.MouseEvent; 
-import javafx.scene.paint.Color; 
-import javafx.scene.shape.Circle; 
-
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import java.util.EventObject;
 import javafx.event.Event;
 import javafx.event.ActionEvent;
-
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.layout.Region;
 import javafx.scene.control.Control;
 import javafx.scene.control.ComboBoxBase;
 import javafx.scene.control.ColorPicker;
-
-import javafx.scene.text.Font; 
+import java.util.Arrays;
+import javafx.scene.input.MouseButton;
+import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.scene.Cursor;
+import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text; 
-import javafx.stage.Stage; 
-         
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.util.Random;
 import java.lang.Object;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -36,7 +44,6 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.Control;
 import javafx.scene.control.ChoiceBox;
-
 import java.lang.Object;
 import javafx.stage.Window;
 import javafx.stage.PopupWindow;
@@ -47,56 +54,63 @@ import javafx.scene.control.MenuItem;
 public class Vertex extends MenuItemArray
 {
 	private static int counter = 0;
-	private int index;
-	private final static double WIDTH = 1500;
-	private final static double HEIGHT = 750;
+	private final static int WIDTH = 1000;
+	private final static int HEIGHT = 500;
 	private final static double RADIUS = 15;
 	public static String[] colorArray = ColorArray.getColorArray();
-	
+	public int[][] ar = RandomNodes.array;
+
+
+
 	public Vertex(){
 		super(RADIUS);
-		setCenterX(Math.random()*WIDTH);
-		setCenterY(Math.random()*HEIGHT);
+		Random rand = new Random();
+			setCenterX(rand.nextInt(WIDTH) + 100);
+			setCenterY(rand.nextInt(HEIGHT) + 15);
+
 		index = counter;
 		counter++;
-
 		ContextMenu colorMenu = new ContextMenu();
 		MenuItem[] menuItemArray = getMenuItemArray();
-		
-		/*
-		This was original template for a menu item that I just kept here
-		Doesn't do anything when uncommented
-		
-		MenuItem item0 = new MenuItem("Cyan");
-	item0.setOnAction((new EventHandler<ActionEvent>(){
-		public void handle(ActionEvent e){
-			Testing.vertex0.setFill(Color.web(colorArray[0]));
-	}}));
-	menuItemArray[0]=item0;
-	*/	
-		
-			
-	
-		for(int i=0; i<91;i++){ //Change 91 to less for smaller menu's with less colors
-		colorMenu.getItems().addAll(menuItemArray[i]);
+
+
+
+
+		for(int i=0; i<51;i++){ //Change 91 to less for smaller menu's with less colors
+			colorMenu.getItems().addAll(menuItemArray[i]);
 		}
-		
+
 		this.setOnMouseClicked((new EventHandler<MouseEvent>(){
-		public void handle(MouseEvent e){
-		PointerInfo a = MouseInfo.getPointerInfo();
-		Point b = a.getLocation();
-		colorMenu.show(Testing.root,b.getX(),b.getY());
-		}}));
-		}
-		
-		/*
-		Not used anymore but might be an idea to add something like this to 
-		the MenuItemArray class to make color checking easier
-		
-		public void setColorIndex(int colorIndex){
-			this.colorIndex = colorIndex;
-			this.setFill(Color.web(colorArray[colorIndex]));
-		}
-		*/
-		
+			public void handle(MouseEvent e){
+				MouseButton butt = e.getButton();
+				if ((Menu.gamemode == 3 && selected == true) || Menu.gamemode < 3) {
+					if (butt == MouseButton.SECONDARY) {
+						PointerInfo a = MouseInfo.getPointerInfo();
+						Point b = a.getLocation();
+						colorMenu.show(Menu.root, b.getX(), b.getY());
+						colorMenu.setHideOnEscape(true);
+					}
+				}
+			}}));
+		setUpDragging(this);
 	}
+	private void setUpDragging(Vertex circle) {
+
+		circle.setOnDragDetected(event -> {
+			circle.getParent().setCursor(Cursor.CLOSED_HAND);
+
+			circle.setCenterX(event.getSceneX());
+			circle.setCenterY(event.getSceneY());
+		});
+
+		circle.setOnMouseReleased(event -> {
+			circle.getParent().setCursor(Cursor.DEFAULT);
+			circle.setCenterX(event.getSceneX());
+			circle.setCenterY(event.getSceneY());
+
+		});
+	}
+
+
+
+}
