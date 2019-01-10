e431e791400892b75b74a65f12313319dc65b9c2
royRLL <34250797+royRLL@users.noreply.github.com>
2019-01-10 13:36:44 +0100

    Create TestingChromatic.java

---
+++ TestingChromatic.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TestingChromatic {
+
+	public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("please input the filename : ");
+    String inputFile = scan.next();
+
+    int[][] adjMat = ChromaticSolver.readCreateMatrix(inputFile);
+    int vertex = adjMat.length;
+
+    int chromaticNum = ChromaticMethods.fromScartchChromatic(adjMat,vertex);
+
+    int clSize = findClicque.findClique(adjMat);
+
+    int chromaticSatu = ChromaticMethods.chromaticSaturation(adjMat);
+
+    System.out.println("The chromatic number is : " + chromaticNum);
+    System.out.println("The Clique size is  : " + clSize);
+    System.out.println("The chromatic (using saturation number is : " + chromaticSatu);
+
+
+
+
+
+
+	}
+}
