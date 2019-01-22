5982fe74465c5429b4457c94538c9f89e0668613
royRLL <34250797+royRLL@users.noreply.github.com>
2019-01-22 22:12:55 +0100

    Update TestingChromatic.java

--- TestingChromatic.java
+++ TestingChromatic.java
@@ -1,38 +1,28 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import Jama.*;
 
-public class TestingChromatic {
-
-	public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    System.out.println("please input the filename : ");
-    String inputFile = scan.next();
-
-    int[][] adjMat = ChromaticSolver.readCreateMatrix(inputFile);
-    int vertex = adjMat.length;
-
-    int chromaticNum = ChromaticMethods.fromScartchChromatic(adjMat,vertex);
-
-    int clSize = findClicque.findClique(adjMat);
-
-    int chromaticSatu = ChromaticMethods.chromaticSaturation(adjMat);
-
-    int randomSaturation = ChromaticMethods.randomSaturationChromatic(adjMat);
-
-    int passSatu = PASS.PASSchromaticSaturation(adjMat);
-    int randomPass = PASS.randomPASS(adjMat);
-    int[] vertexOrdering = findClicque.vertexOrdering(adjMat);
-    int[] degreesOrdering = findClicque.degreesOrdering(adjMat,vertexOrdering);
+public class TestingChromatic   {
+    public static int[][] aMatrix;
+    public static int chromatic;
+    public static int upperB;
 
 
-    System.out.println("The chromatic number is : " + chromaticNum);
-//    System.out.println("The Clique size is  : " + clSize);
-    System.out.println("The chromatic (using saturation) number is : " + chromaticSatu);
-    System.out.println("The chromatic (using random saturation) number is : " + randomSaturation);
-    System.out.println("The chromatic (using PASS) number is : " + passSatu);
-    System.out.println("The chromatic (using random PASS) number is : " + randomPass);
-    System.out.println(Arrays.toString(degreesOrdering));
-    System.out.println(Arrays.toString(vertexOrdering));
 
-	}
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("please input the filename : ");
+        String inputFile = scan.next();
+
+        int[][] adjMat = ChromaticSolver.readCreateMatrix(inputFile);
+        aMatrix = adjMat;
+        int vertex = adjMat.length;
+        int chromaticNum = ChromaticMethods.fromScartchChromatic(adjMat, vertex);
+        chromatic = chromaticNum;
+
+        System.out.println("The chromatic number is : " + chromatic);
+
+
+    }
 }
